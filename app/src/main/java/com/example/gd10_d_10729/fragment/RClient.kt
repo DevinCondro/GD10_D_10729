195a6217819f1286dbde5013cc9d853af3111342
Devin <devincondro123@gmail.com>
2022-11-08 10:07:15 +0700

    Pengerjaan Guided 10

---
+++ app/src/main/java/com/example/gd10_d_10729/fragment/RClient.kt
@@ -0,0 +1,22 @@
+package com.example.gd10_d_10729.fragment
+
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+
+object RClient {
+
+
+    private const val BASE_URL = "http://10.53.15.229/ci4-apiserver/public/"
+    val instances: api by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(api::class.java)
+    }
+}
